d8bba2b4cd07fb067426f5ff86a1580bee887cf8
alp <76008113+interdict0r@users.noreply.github.com>
2024-08-02 03:58:53 +0300

    Add files via upload

---
+++ mgr/mgr.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <atomic>
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
+#include <Windows.h>
