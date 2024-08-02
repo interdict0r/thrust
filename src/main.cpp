c0ab346fa5aa3acc9378c4018f4a4ad21ab680e7
alp <76008113+interdict0r@users.noreply.github.com>
2024-08-02 03:40:50 +0300

    added thread pool, job queue
    implemented thread pool
    implemented job queue
    drastically improved multithreading performance (i think)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
-#include <Windows.h>
+#include <atomic>
+#include <condition_variable>
 #include <iostream>
+#include <mutex>
+#include <queue>
 #include <string>
 #include <thread>
 #include <vector>
-#include <mutex>
+#include <Windows.h>
 
 std::mutex cout_mutex;
+std::mutex queue_mutex;
+std::condition_variable cv;
+std::atomic<bool> done(false);
+std::queue<std::string> file_queue;
+std::queue<std::string> dir_queue;
+
 
 BOOL IsProcessElevated()
 {
@@ -32,58 +41,114 @@ Cleanup:
 	return fIsElevated;
 }
 
+void WorkerThread() noexcept
+{
+	while (true)
+	{
+		std::string path;
+		{
+			std::unique_lock<std::mutex> lock(queue_mutex);
+			cv.wait(lock, [] { return !file_queue.empty() || !dir_queue.empty() || done.load(); });
+
+			if (file_queue.empty() && dir_queue.empty() && done.load())
+				return;
+
+			if (!file_queue.empty())
+			{
+				path = file_queue.front();
+				file_queue.pop();
+			}
+			else if (!dir_queue.empty())
+			{
+				path = dir_queue.front();
+				dir_queue.pop();
+			}
+		}
+
+		if (!path.empty())
+		{
+			DWORD dwFileAttribute = GetFileAttributes(path.c_str());
+
+			if (dwFileAttribute & FILE_ATTRIBUTE_READONLY)
+			{
+				dwFileAttribute &= ~FILE_ATTRIBUTE_READONLY;
+				SetFileAttributes(path.c_str(), dwFileAttribute);
+			}
+
+			if (dwFileAttribute & FILE_ATTRIBUTE_DIRECTORY)
+			{
+				if (RemoveDirectory(path.c_str()))
+				{
+					std::lock_guard<std::mutex> lock(cout_mutex);
+					std::cout << "Deleted directory: [" << path << "]" << '\n';
+				}
+				else
+				{
+					std::lock_guard<std::mutex> lock(cout_mutex);
+					std::cerr << "Failed to delete directory: [" << path << "]" << '\n';
+				}
+			}
+			else
+			{
+				if (DeleteFile(path.c_str()))
+				{
+					std::lock_guard<std::mutex> lock(cout_mutex);
+					std::cout << "Deleted file: [" << path << "]" << '\n';
+				}
+				else
+				{
+					std::lock_guard<std::mutex> lock(cout_mutex);
+					std::cerr << "Failed to delete file: [" << path << "]" << '\n';
+				}
+			}
+		}
+	}
+}
 
 void DeleteAllFiles(const std::string& strPath) noexcept
 {
 	WIN32_FIND_DATA wfd;
-	std::string strSpec = strPath + "*.*";
+	std::string strSpec = strPath + "\\*.*";
 	HANDLE hFile = FindFirstFile(strSpec.c_str(), &wfd);
 
 	if (hFile != INVALID_HANDLE_VALUE)
 	{
 		std::vector<std::thread> threads;
+		const unsigned int num_threads = std::thread::hardware_concurrency();
+
+		for (int i = 0; i < num_threads; i++)
+			threads.emplace_back(WorkerThread);
 
 		do
 		{
 			std::string strFile = wfd.cFileName;
-			std::string strPathFile = strPath + strFile;
-			DWORD dwFileAttribute = GetFileAttributes(strPathFile.c_str());
-
-			if (dwFileAttribute & FILE_ATTRIBUTE_READONLY)
-			{
-				dwFileAttribute &= ~FILE_ATTRIBUTE_READONLY;
-				SetFileAttributes(strPathFile.c_str(), dwFileAttribute);
-			}
+			std::string strPathFile = strPath + "\\" + strFile;
 
 			if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 			{
 				if (strcmp(strFile.c_str(), ".") != 0 && strcmp(strFile.c_str(), "..") != 0)
 				{
-					strPathFile += "\\";
-					threads.emplace_back(DeleteAllFiles, strPathFile);
-
-					std::lock_guard<std::mutex> lock(cout_mutex);
-					if (RemoveDirectory(strPathFile.c_str()))
-						std::cout << "Deleted directory: [" << strPathFile << "]" << '\n';
-					else
-						std::cerr << "Failed to delete directory: [" << strPathFile << "]" << '\n';
+					std::string nestedPath = strPathFile + "\\";
+					DeleteAllFiles(nestedPath);
+					{
+						std::lock_guard<std::mutex> lock(queue_mutex);
+						dir_queue.push(strPathFile);
+					}
+					cv.notify_one();
 				}
 			}
 			else
 			{
-				threads.emplace_back([strPathFile]()
-				{
-					std::lock_guard<std::mutex> lock(cout_mutex);
-					if (DeleteFile(strPathFile.c_str()))
-						std::cout << "Deleted file: [" << strPathFile << "]" << '\n';
-					else
-						std::cerr << "Failed to delete file: [" << strPathFile << "]" << '\n';
-				});
+				std::lock_guard<std::mutex> lock(queue_mutex);
+				file_queue.push(strPathFile);
+				cv.notify_one();
 			}
 		}
 		while (FindNextFile(hFile, &wfd));
 
 		FindClose(hFile);
+		done.store(true);
+		cv.notify_all();
 
 		for (auto& thread : threads)
 		{
@@ -112,7 +177,6 @@ void StartDeletionThreads(const std::vector<std::string>& paths) noexcept
 int main(void*)
 {
 	SetConsoleTitleW(L"THRUST");
-	IsProcessElevated();
 
 	if (!IsProcessElevated())
 	{
@@ -126,7 +190,7 @@ int main(void*)
 
 	while (true)
 	{
-		std::cout << "      THRUST - Optimization Service\n------------------------\n";
+		std::cout << "      THRUST Optimization Service\n------------------------\n";
 		std::cout << " 1 - Exit\n 2 - CleanMgr\n 3 - ChkDsk (doesn't work as administrator)\n 4 - Prune\n";
 		std::cout << "Enter corresponding character.\n";
 		std::cin >> idx;
